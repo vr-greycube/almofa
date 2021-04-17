363204008eddc9b23c048540364a45b13393c552
ashish-greycube <mr.ashish.shah@gmail.com>
2021-04-17 07:35:54 +0200

    feat:sales man visit doctype added

---
+++ almofa/almofa/doctype/sales_man_visit/sales_man_visit.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Greycube and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Man Visit', {
+	// refresh: function(frm) {
+
+	// }
+});
